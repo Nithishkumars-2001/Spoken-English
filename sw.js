93c9bc7d1785e0b874f005e2dacbf801f168b560
Nithish Kumar <nithishkumar70102@gmail.com>
2026-07-03 10:06:12 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 11232179
+    "domain": "3nbf4.com",
+    "zoneId": 11232214
 }
 self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
